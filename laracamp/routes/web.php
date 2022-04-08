99ec729093e7fe67cd672ef8f10c3d67c3532c82
eunikehi <eunikehi77@gmail.com>
2022-04-08 17:50:08 +0700

    [integration] checkout part 1

--- laracamp/routes/web.php
+++ laracamp/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,13 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('checkout/{camp:slug}', function () {
-    return view('checkout');
-})->name('checkout');
+// Route::get('checkout/{camp:slug}', function () {
+//     return view('checkout');
+// })->name('checkout');
 
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+Route::get('checkout/success}', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+
+// Route::get('success-checkout', function () {
+//     return view('success_checkout');
+// })->name('success-checkout');
 
 // socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
