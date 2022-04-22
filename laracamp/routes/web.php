522a4a32b187c413aa017f4c8cbdbf06f9cb502d
eunikehi <eunikehi77@gmail.com>
2022-04-23 00:24:32 +0700

    slicing & integration my Dashboard

--- laracamp/routes/web.php
+++ laracamp/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\User\CheckoutController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,24 +20,20 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-// Route::get('checkout/{camp:slug}', function () {
-//     return view('checkout');
-// })->name('checkout');
-
-Route::get('checkout/success}', [CheckoutController::class, 'success'])->name('checkout.success');
-Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
-Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');   
-
-// Route::get('success-checkout', function () {
-//     return view('success_checkout');
-// })->name('success-checkout');
-
 // socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+   // checkout routes
+   Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success')->middleware('ensureUserRole:user');
+   Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create')->middleware('ensureUserRole:user');
+   Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store')->middleware('ensureUserRole:user');
+   // dashboard
+   Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard'); 
+});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
