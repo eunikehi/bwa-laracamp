54daec64ad822fb71a2ad29eedce98830dbf1d0a
eunikehi <eunikehi77@gmail.com>
2022-05-03 19:55:59 +0700

    admin email paid

--- laracamp/app/Http/Controllers/Admin/CheckoutController.php
+++ laracamp/app/Http/Controllers/Admin/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Checkout;
+use Mail;
+use App\Mail\Checkout\Paid;
 
 class CheckoutController extends Controller
 {
@@ -13,6 +15,8 @@ class CheckoutController extends Controller
         $checkout->is_paid = true;
         $checkout->save();
         $request->session()->flash('success', "Checkout with ID {$checkout->id} has been updated!");
+        // send email to user
+        Mail::to($checkout->User->email)->send(new Paid($checkout));
         return redirect(route('admin.dashboard'));
     }
 }
